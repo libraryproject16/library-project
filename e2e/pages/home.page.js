d5b848f4626350a3105eefc55abfe4bd2105ccf1
Kenny Gray <kgray@scottlogic.co.uk>
2016-02-02 16:54:07 +0000

    Added angular 2, moved over to typescript, added e2e home.spec

--- e2e/pages/home.page.js
+++ e2e/pages/home.page.js
@@ -1,11 +1,14 @@
-var serverAddress = require('../helpers/server-address');
 var homePage = Object.create({}, {
-    navigate: { value: navigate }
+    navigate: { value: navigate },
+    heading: { get: getHeading }
 });
 
 module.exports = homePage;
 
 function navigate() {
-    // TODO: When angular exists: browser.get(serverAddress);
-    browser.driver.get(serverAddress);
+    browser.get('');
+}
+
+function getHeading() {
+    return element(by.tagName('h1'));
 }
--- tools/protractor.conf.js
+++ tools/protractor.conf.js
@@ -1,3 +1,7 @@
+var serverAddress = require('./server-address');
+
 exports.config = {
+    baseUrl: serverAddress,
+    rootElement: 'app',
     specs: ['e2e/**/*.spec.js']
 };
